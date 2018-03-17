d43af99264cfe508600248149cc33c9022979a1c
A-Aron <vale.aaron@gmail.com>
2018-03-17 15:23:09 -0400

    Get Working Graph BFS

--- public/scripts/bfs.js
+++ public/scripts/bfs.js
@@ -1,122 +1,101 @@
-function PriorityQueue () {
-  this._nodes = [];
-
-  this.enqueue = function (priority, key) {
-    this._nodes.push({key: key, priority: priority });
-    this.sort();
-  };
-  this.dequeue = function () {
-    return this._nodes.shift().key;
-  };
-  this.sort = function () {
-    this._nodes.sort(function (a, b) {
-      return a.priority - b.priority;
-    });
-  };
-  this.isEmpty = function () {
-    return !this._nodes.length;
-  };
+function Graph() {
+  this.nodes = [];
+  this.graph = {};
+  this.end = null;
+  this.start = null;
 }
 
-/**
- * Pathfinding starts here
- */
-function Graph(){
-  var INFINITY = 1/0;
-  this.vertices = {};
-
-  this.addVertex = function(name, edge){
-    if (!this.vertices[name]) {
-      this.vertices[name] = [edge]
-    } else {
-      this.vertices[name].push(edge);
-    }
-  };
-
-	this.shortestPath = function (graph, source, target) {
-                      // the target.
-  var queue = [ source ],
-      visited = { source: true },
-      predecessor = {},
-      tail = 0;
-  while (tail < queue.length) {
-    var u = queue[tail++],  // Pop a vertex off the queue.
-        neighbors = graph.vertices[u];
-    for (var i = 0; i < neighbors.length; ++i) {
-      var v = neighbors[i];
-      if (visited[v]) {
-        continue;
-      }
-      visited[v] = true;
-      if (v === target) {   // Check if the path is complete.
-        var path = [ v ];   // If so, backtrack through the path.
-        while (u !== source) {
-          u = predecessor[u];
-          path.push(u);
-        }
-        path.reverse();
-        console.log(path.join(' &rarr; '));
-        return;
-      }
-      predecessor[v] = u;
-      queue.push(v);
-    }
+Graph.prototype.reset = function() {
+  for (var i = 0; i < this.nodes.length; i++) {
+    this.nodes[i].searched = false;
+    this.nodes[i].parent = null;
   }
-  console.log('there is no path from ' + source + ' to ' + target);
 }
 
-  // this.shortestPath = function (start, finish) {
-  //   var nodes = new PriorityQueue(),
-  //       distances = {},
-  //       previous = {},
-  //       path = [],
-  //       smallest, vertex, neighbor, alt;
-  //   for(vertex in this.vertices) {
-  //     if(vertex === start) {
-  //       distances[vertex] = 0;
-  //       nodes.enqueue(0, vertex);
-  //     }
-  //     else {
-  //       distances[vertex] = INFINITY;
-  //       nodes.enqueue(INFINITY, vertex);
-  //     }
-  //
-  //     previous[vertex] = null;
-  //   }
-  //
-  //   while(!nodes.isEmpty()) {
-  //     smallest = nodes.dequeue();
-  //
-  //     if(smallest === finish) {
-  //       path = [];
-  //
-  //       while(previous[smallest]) {
-  //         path.push(smallest);
-  //         smallest = previous[smallest];
-  //       }
-  //
-  //       break;
-  //     }
-  //
-  //     if(!smallest || distances[smallest] === INFINITY){
-  //       continue;
-  //     }
-  //
-  //     for(neighbor in this.vertices[smallest]) {
-  //       alt = distances[smallest] + this.vertices[smallest][neighbor];
-  //
-  //       if(alt < distances[neighbor]) {
-  //         distances[neighbor] = alt;
-  //         previous[neighbor] = smallest;
-  //
-  //         nodes.enqueue(alt, neighbor);
-  //       }
-  //     }
-  //   }
-	// 	console.log(distances)
-	// 	console.log(nodes)
-  //   return path;
-  // };
+Graph.prototype.setStart = function(node) {
+  this.start = this.graph[node];
+  return this.start;
+}
+
+Graph.prototype.setEnd = function(node) {
+  this.end = this.graph[node];
+  return this.end;
+}
+
+
+Graph.prototype.addNode = function(n) {
+  // Node into array
+  this.nodes.push(n);
+  var title = n.value;
+  // Node into "hash"
+  this.graph[title] = n;
+}
+
+Graph.prototype.getNode = function(node) {
+  var n = this.graph[node];
+  return n;
+}
+
+Graph.prototype.shortestPath = function (start, finish) {
+	this.start = start;
+	this.end = finish;
+	var queue = [];
+
+
+	this.start.searched = true;
+	queue.push(this.start);
+
+	while (queue.length > 0) {
+		var current = queue.shift();
+		if (current === finish) {
+			console.log("Found " + current.value);
+			break;
+		}
+		var edges = current.edges;
+		if (edges) {
+
+			for (var i = 0; i < edges.length; i++) {
+				var neighbor = edges[i];
+				if (!neighbor.searched) {
+					neighbor.searched = true;
+					neighbor.parent = current;
+					queue.push(neighbor);
+				}
+			}
+		}
+	}
+
+	var path = [];
+	path.push(finish);
+	var next = finish.parent;
+	while (next != null) {
+		path.push(next);
+		next = next.parent;
+	}
+
+	var txt = '';
+	for (var i = path.length - 1; i >= 0; i--) {
+		var n = path[i];
+		txt += n.value
+		if (i != 0) {
+			txt += ' --> '
+		};
+	}
+	return path
+};
+
+
+function Node(value) {
+  this.value = value;
+  this.edges = [];
+  this.searched = false;
+  this.parent = null;
+}
+
+Node.prototype.addEdge = function(neighbor) {
+  this.edges.push(neighbor);
+  // Both directions!
+  neighbor.edges.push(this);
 }
 
 var g = new Graph();
@@ -126,15 +105,35 @@ Papa.parse("https://raw.githubusercontent.com/Aaron-Vale/Six-Degrees-of-Kevin-Yo
 	download: true,
   header: false,
 	complete: function(results) {
-		for (let i = 0; i < results.data.length; i++) {
-      let node = results.data[i];
+		// for (let i = 0; i < results.data.length; i++) {
+    //   let node = results.data[i];
+    //   let name = node[1];
+    //   let team = `${node[2]} ${node[4]}`;
+		// 	if (name && team) {
+		// 		g.addVertex(name, team);
+		// 		g.addVertex(team, name);
+		// 	}
+
+		for (var i = 0; i < results.data.length; i++) {
+		  let node = results.data[i];
       let name = node[1];
       let team = `${node[2]} ${node[4]}`;
-			if (name && team) {
-				g.addVertex(name, team);
-				g.addVertex(team, name);
+			let playerNode;
+			let teamNode;
+			if (!g.getNode(name)) {
+				playerNode = new Node(name);
+				g.addNode(playerNode);
+			} else {
+				playerNode = g.getNode(name);
+			}
+			if (!g.getNode(team)) {
+				teamNode = new Node(team);
+				g.addNode(teamNode);
+			} else {
+				teamNode = g.getNode(team);
 			}
 
+			teamNode.addEdge(playerNode);
 
       // if(g.vertices[name]) {
       //   g.addVertex(name, team);
@@ -145,6 +144,6 @@ Papa.parse("https://raw.githubusercontent.com/Aaron-Vale/Six-Degrees-of-Kevin-Yo
       // }
     }
     console.log(g);
-    console.log(g.shortestPath(g, 'Andrew Benintendi', 'Dustin Pedroia'));
+    console.log(g.shortestPath(g.getNode('Dustin Pedroia'), g.getNode('Manny Ramirez')));
 	}
 });
